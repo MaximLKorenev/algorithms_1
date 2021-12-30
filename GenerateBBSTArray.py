b5f0c6d16b0c6ea7e2111cb1e4e17006e825460c
MaximLKorenev <mlkorenev@yandex.ru>
2021-12-30 15:13:29 +0300

    new

---
+++ GenerateBBSTArray.py
@@ -0,0 +1,16 @@
+def GenerateBBSTArray(a):
+    a.sort()
+    abst = [None] * len(a)
+    start = 0
+
+    def sort_for_bbst(a_bst, start_index, list_to_sort):
+        middle = len(list_to_sort) // 2
+        a_bst[start_index] = list_to_sort[middle]
+        if len(list_to_sort) == 1:
+            return
+        start_left = start * 2 + 1
+        start_right = start * 2 + 2
+        sort_for_bbst(a_bst, start_left, list_to_sort[:middle])
+        sort_for_bbst(a_bst, start_right, list_to_sort[middle + 1:])
+    sort_for_bbst(abst, start, a)
+    return abst
