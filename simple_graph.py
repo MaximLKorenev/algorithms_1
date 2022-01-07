6522c16acbba9b02f81b58c5b2ce3752e4c3f0ac
MaximLKorenev <mlkorenev@yandex.ru>
2022-01-07 08:33:43 +0300

    new

---
+++ simple_graph.py
@@ -0,0 +1,48 @@
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+
+
+class SimpleGraph:
+
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v):
+        vert = Vertex(v)
+        for i in range(self.max_vertex):
+            if self.vertex[i] is None:
+                self.vertex[i] = vert
+                return
+
+        # здесь и далее, параметры v -- индекс вершины
+
+    # в списке  vertex
+    def RemoveVertex(self, v):
+        if v >= self.max_vertex:
+            return
+        for v2 in range(self.max_vertex):
+            if self.IsEdge(v, v2):
+                self.RemoveEdge(v, v2)
+        self.vertex[v] = None
+
+    def IsEdge(self, v1, v2):
+        # True если есть ребро между вершинами v1 и v2
+        if v1 < self.max_vertex and v2 < self.max_vertex:
+            return self.m_adjacency[v1][v2] == 1
+        return False
+
+    def AddEdge(self, v1, v2):
+        # добавление ребра между вершинами v1 и v2
+        if v1 < self.max_vertex and v2 < self.max_vertex:
+            self.m_adjacency[v1][v2] = 1
+            self.m_adjacency[v2][v1] = 1
+
+    def RemoveEdge(self, v1, v2):
+        # удаление ребра между вершинами v1 и v2
+        if v1 < self.max_vertex and v2 < self.max_vertex:
+            self.m_adjacency[v1][v2] = 0
+            self.m_adjacency[v2][v1] = 0
